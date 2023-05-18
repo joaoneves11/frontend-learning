cafa4ccf67bbd06ea8597e124bd74c6284179c6c
João Neves <joaopedro.n@outlook.com.br>
2023-05-18 18:46:43 -0300

    express view

---
+++ node/aula10/server.js
@@ -0,0 +1,21 @@
+//carregando express
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+const path = require('path')
+app.use(routes)
+app.use(
+    express.urlencoded(
+        {
+            extended: true
+        }
+    )
+)
+app.set('views', path.resolve(__dirname, 'src', 'views')) //buscando o arquivo de view
+app.set('view engine', 'ejs')
+
+
+app.listen(3000, () =>{
+    console.log('acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000')
+})
